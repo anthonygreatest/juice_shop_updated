97302fea28dba74142e414ebccc819e5505f05a0
user <grig.2001@list.ru>
2026-01-28 18:23:44 +0300

    updates for ci

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / '.env',
         env_file_encoding='utf-8',
-        env_nested_delimiter='.'
+        env_nested_delimiter='__'
     )
 
     shop_http_client: HTTPClientConfig
