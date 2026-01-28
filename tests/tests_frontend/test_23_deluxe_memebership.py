224134b688c9862e25d1770c5736c5eb7e3a260f
user <grig.2001@list.ru>
2026-01-28 18:19:32 +0300

    updates for ci

--- tests/tests_frontend/test_23_deluxe_memebership.py
+++ tests/tests_frontend/test_23_deluxe_memebership.py
@@ -1,12 +1,11 @@
 import allure
 import pytest
-
 from data.frontend_endpoints import PlaywrightEndpoints
 from data.generators.order_generator import pick_product_to_purchase
-from pages.login_page import LoginPage
-from tests.tests_frontend.frontend_helpers import log_in_user, log_out_and_log_in, find_out_product_price
+from tests.tests_frontend.frontend_helpers import log_out_and_log_in, find_out_product_price
 from utils.assertions.basket_assertions import assert_price_changes_for_deluxe_member
 from utils.assertions.e_wallet_assertions import assert_balance_changes_after_paying_membership_fee
+
 
 @pytest.mark.smoke
 @pytest.mark.usefixtures('close_cookies_banner')
@@ -14,8 +13,14 @@ from utils.assertions.e_wallet_assertions import assert_balance_changes_after_pa
 @allure.story('Valid deluxe membership flow')
 @allure.title('Membership text appears on page and fee is paid')
 class TestDeluxeMembership:
-    def test_membership_text_appears_on_page_and_fee_is_paid(self, deluxe_membership_page, sum_deposited_into_e_wallet,
-        digital_wallet, payment_options_page, get_headers):
+    def test_membership_text_appears_on_page_and_fee_is_paid(
+        self,
+        deluxe_membership_page,
+        sum_deposited_into_e_wallet,
+        digital_wallet,
+        payment_options_page,
+        get_headers
+    ):
 
         deposit, balance_before_deposit = sum_deposited_into_e_wallet
 
@@ -38,8 +43,13 @@ class TestDeluxeMembership:
         )
 
     @pytest.mark.delete_product
-    def test_deluxe_membership_offers_sales(self, put_product_into_basket, basket_page, user_register_data_via_ui,
-        deluxe_membership_page):
+    def test_deluxe_membership_offers_sales(
+        self,
+        put_product_into_basket,
+        basket_page,
+        user_register_data_via_ui,
+        deluxe_membership_page
+    ):
 
         log_out_and_log_in(
             current_page=deluxe_membership_page,
@@ -61,9 +71,17 @@ class TestDeluxeMembership:
     @pytest.mark.delete_card
     @pytest.mark.delete_address
     @pytest.mark.delete_product
-    def test_deluxe_membership_offers_free_fast_delivery(self, get_headers, create_address, put_product_into_basket,
-        checkout_page, user_register_data_via_ui, deluxe_membership_page, payment_options_page,
-        navigate_to_payment_options_page):
+    def test_deluxe_membership_offers_free_fast_delivery(
+        self,
+        get_headers,
+        create_address,
+        put_product_into_basket,
+        checkout_page,
+        user_register_data_via_ui,
+        deluxe_membership_page,
+        payment_options_page,
+        navigate_to_payment_options_page
+    ):
 
         create_address(headers=get_headers)
 
