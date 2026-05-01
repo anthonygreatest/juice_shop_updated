01ec6057296df66c2cbc6bfc6af4afde14a06f95
user <grig.2001@list.ru>
2026-05-01 23:27:49 +0300

    New try

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from pydantic import BaseModel, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class HTTPClientConfig(BaseModel):
-
-    url: HttpUrl
-    timeout: float
-
-    @property
-    def client_url(self) -> str:
-        return str(self.url)
+# class HTTPClientConfig(BaseModel):
+#
+#     url: HttpUrl
+#     timeout: float
+#
+#     @property
+#     def client_url(self) -> str:
+#         return str(self.url)
 
 BASE_DIR = Path(__file__).parent
 
@@ -20,10 +20,16 @@ class Settings(BaseSettings):
 
     # model_config = SettingsConfigDict(
     #     env_file=BASE_DIR / '.env',
-    #     env_file_encoding='utf-8'
+    #     env_file_encoding='utf-8',
+    #     env_nested_delimiter="_"
     # )
 
-    shop_http_client: HTTPClientConfig
+    shop_http_client_url: HttpUrl
+    shop_http_client_timeout: float
+
+    @property
+    def client_url(self) -> str:
+        return str(self.url)
 
 
 #
